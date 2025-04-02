5bd4fa877fe8d2cc511918b99f7fe2b2cc5ec668
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-02 12:30:46 +0300

    Test

---
+++ Migrations/20250329185045_ProductsSeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductsSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 10000m, "Computer" },
+                    { 2, 100m, "Mouse" },
+                    { 3, 50000m, "Laptop" },
+                    { 4, 5000m, "Printer" },
+                    { 5, 20000m, "Keyboard" },
+                    { 6, 30000m, "Screen" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+SQLitePCL.Batteries.Init();
+
+builder.Services.AddDbContext<RepositoryContext>(options=>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
+
+builder.Services.AddControllersWithViews(); 
+var app = builder.Build();
+
+app.UseStaticFiles(); // for the usage of static files configuration (wwwroot)
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name:"Default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
