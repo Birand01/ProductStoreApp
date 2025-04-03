43b8f958664499817608b350031611e8a36ed370
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-03 21:56:38 +0300

    Category class is created

--- obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ba54be4cb962174071925277a590cee0ad6ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8ba2be152d5ed38bd9111f579ce72a1b9ecf9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
