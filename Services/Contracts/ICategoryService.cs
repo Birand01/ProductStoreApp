0c100838139123872f29ae6b2098cbc11e17dd59
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-04 08:10:19 +0300

    Product manager is implemented

---
+++ Services/Contracts/ICategoryService.cs
@@ -0,0 +1,13 @@
+using Entities.Models;
+
+namespace Services.Contracts
+{
+    public interface ICategoryService
+    {
+        IEnumerable<Category> GetAllCategories(bool trackChanges);
+    }
+
+
+
+
+}
