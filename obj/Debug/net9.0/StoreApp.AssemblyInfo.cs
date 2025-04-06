82f8d9a817fad52a1037c67c249d231ed86706c7
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-06 08:47:33 +0300

    Admin panel view is re organized

--- obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63927e0e8f0538f62ed9b85e6503cf92be336004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9020b8859dd00ac498de9fc2976ab42e14c5b1e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
