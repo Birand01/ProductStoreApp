7f787d83c14863f04973de1dba81a4a667037164
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-06 10:49:29 +0300

    Create page is implemented in product list admin panel

--- obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f8d9a817fad52a1037c67c249d231ed86706c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d242a9c2d896b2389f37d9b1d6787d45db615e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
